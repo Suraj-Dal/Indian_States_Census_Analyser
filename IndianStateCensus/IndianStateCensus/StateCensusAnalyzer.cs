c8c03d91b66b5f67d3a6e412092fcc748da9cc7b
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-24 13:14:51 +0530

    [Suraj]: Usecase 2 to load State code data from CSV file is added.

--- IndianStateCensus/IndianStateCensus/StateCensusAnalyzer.cs
+++ IndianStateCensus/IndianStateCensus/StateCensusAnalyzer.cs
@@ -16,7 +16,7 @@ namespace IndianStateCensus
             {
                 try
                 {
-                    if (filePath.Contains("IncorrectHeaderCensusData.csv"))
+                    if (filePath.Contains("StateCensusData.csv"))
                     {
                         int count;
                         using (var reader = new StreamReader(filePath))
@@ -28,6 +28,41 @@ namespace IndianStateCensus
                             foreach (var data in list)
                             {
                                 Console.WriteLine(data.State + "\t-->\t" + data.Population + "\t-->\t" + data.AreaInSqKm + "\t-->\t" + data.DensityPerSqKm);
+                            }
+                        }
+                        return count;
+                    }
+                    throw new ExceptionHandler(ExceptionHandler.ExceptionType.INVALID_FILE, "Invalid File");
+                }
+                catch (CsvHelper.MissingFieldException)
+                {
+                    throw new ExceptionHandler(ExceptionHandler.ExceptionType.INCORRECT_DELIMITER, "Incorrect Delimiter");
+                }
+                catch (CsvHelper.HeaderValidationException)
+                {
+                    throw new ExceptionHandler(ExceptionHandler.ExceptionType.INCORRECT_HEADER, "Incorrect Header");
+                }
+            }
+            throw new ExceptionHandler(ExceptionHandler.ExceptionType.INVALID_FILE_TYPE, "Invalid file Type");
+        }
+        public int StateCodeAnalyzer(string filePath)
+        {
+            if (Path.GetExtension(filePath) == ".csv")
+            {
+                try
+                {
+                    if (filePath.Contains("StateCode.csv"))
+                    {
+                        int count;
+                        using (var reader = new StreamReader(filePath))
+                        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                        {
+                            var list = csv.GetRecords<StateCodeModel>().ToList();
+                            count = list.Count();
+                            Console.WriteLine("SrNo\t-->\tState\t-->\tName\t-->\tTIN\t-->\tStateCode");
+                            foreach (var data in list)
+                            {
+                                Console.WriteLine(data.SrNo + "\t-->\t" + data.State + "\t-->\t" + data.Name + "\t-->\t" + data.TIN +"\t-->\t"+ data.StateCode);
                             }
                         }
                         return count;
