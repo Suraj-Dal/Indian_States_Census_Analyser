30ed5955777fe1765d163f9c4e73c2250000ad4d
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-24 11:54:05 +0530

    [Suraj]: Testcase 1.2 to to pass invalid file to check thrown exceptio with expected exception is added.

--- IndianStateCensus/AnalyzerTest/UnitTest1.cs
+++ IndianStateCensus/AnalyzerTest/UnitTest1.cs
@@ -8,7 +8,22 @@ namespace AnalyzerTest
             int expected = 29;
             IndianStateCensus.StateCensusAnalyzer analyzer = new IndianStateCensus.StateCensusAnalyzer();
             int result = analyzer.DataAnalyzer(@"C:\Projects\Indian_States_Census_Analyser\StateCensusData.csv");
-            Assert.AreEqual(expected,result);
+            Assert.AreEqual(expected, result);
+        }
+        [Test]//UC1.2 Given invalid file
+        public void GivenInvaliFile_ThrowCostomExceptio_InvaliFile()
+        {
+            try
+            {
+                int expected = 29;
+                IndianStateCensus.StateCensusAnalyzer analyzer = new IndianStateCensus.StateCensusAnalyzer();
+                int result = analyzer.DataAnalyzer(@"C:\Projects\Indian_States_Census_Analyser\CensusData.csv");
+                Assert.AreEqual(expected, result);
+            }
+            catch(Exception ex)
+            {
+                Assert.AreEqual("Invalid File", ex.Message);
+            }
         }
     }
 }
