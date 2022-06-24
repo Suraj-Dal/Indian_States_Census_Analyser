cc7ede8f3162426d90d205ae5075548cbd0aa4b4
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-24 13:23:13 +0530

    [Suraj]: Usecase 2 is added again.

--- IndianStateCensus/IndianStateCensus/Program.cs
+++ IndianStateCensus/IndianStateCensus/Program.cs
@@ -1,3 +1,22 @@
 ﻿IndianStateCensus.StateCensusAnalyzer analyzer = new IndianStateCensus.StateCensusAnalyzer();
-int count = analyzer.DataAnalyzer(@"C:\Projects\Indian_States_Census_Analyser\StateCensusData.csv");
-Console.WriteLine("No. of records in file are:" + count);
+Console.WriteLine("0.Exit\n1.State Sensus Data\n2.State Code\nEnter your choice:");
+int choice = Convert.ToInt32(Console.ReadLine());
+while (choice != 0)
+{
+    switch (choice)
+    {
+        case 1:
+            int countRecords = analyzer.DataAnalyzer(@"C:\Projects\Indian_States_Census_Analyser\StateCensusData.csv");
+            Console.WriteLine("No. of records in file are:" + countRecords);
+            break;
+        case 2:
+            int noOfRecords = analyzer.StateCodeAnalyzer(@"C:\Projects\Indian_States_Census_Analyser\StateCode.csv");
+            Console.WriteLine("No. of records in file are:" + noOfRecords);
+            break;
+        default:
+            Console.WriteLine("Enter valid choice");
+            break;
+    }
+    Console.WriteLine("0.Exit\n1.State Sensus Data\n2.State Code\nEnter your choice:");
+    choice = Convert.ToInt32(Console.ReadLine());
+}
--- IndianStateCensus/IndianStateCensus/StateCensusAnalyzer.cs
+++ IndianStateCensus/IndianStateCensus/StateCensusAnalyzer.cs
@@ -45,5 +45,40 @@ namespace IndianStateCensus
             }
             throw new ExceptionHandler(ExceptionHandler.ExceptionType.INVALID_FILE_TYPE, "Invalid file Type");
         }
+        public int StateCodeAnalyzer(string filePath)
+        {
+            if (Path.GetExtension(filePath) == ".csv")
+            {
+                try
+                {
+                    if (filePath.Contains("StateCodeData.csv"))
+                    {
+                        int count;
+                        using (var reader = new StreamReader(filePath))
+                        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                        {
+                            var list = csv.GetRecords<StateCodeModel>().ToList();
+                            count = list.Count();
+                            Console.WriteLine("SrNo\t-->\tState\t-->\tName\t-->\tTIN\t-->\tStateCode");
+                            foreach (var data in list)
+                            {
+                                Console.WriteLine(data.SrNo + "\t-->\t" + data.State + "\t-->\t" + data.Name + "\t-->\t" + data.TIN + "\t-->\t" + data.StateCode);
+                            }
+                        }
+                        return count;
+                    }
+                    throw new ExceptionHandler(ExceptionHandler.ExceptionType.INVALID_FILE, "Invalid File");
+                }
+                catch (CsvHelper.MissingFieldException)
+                {
+                    throw new ExceptionHandler(ExceptionHandler.ExceptionType.INCORRECT_DELIMITER, "Incorrect Delimiter");
+                }
+                catch (CsvHelper.HeaderValidationException)
+                {
+                    throw new ExceptionHandler(ExceptionHandler.ExceptionType.INCORRECT_HEADER, "Incorrect Header");
+                }
+            }
+            throw new ExceptionHandler(ExceptionHandler.ExceptionType.INVALID_FILE_TYPE, "Invalid file Type");
+        }
     }
 }
